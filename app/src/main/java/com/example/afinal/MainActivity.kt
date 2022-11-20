14faefb8ff5566f554be63e9c9a5e125eb68ae98
Alexander Burgos <axb19c@acu.edu>
2022-11-20 00:33:42 -0600

    Adds function to pull user details upon login and displaying a message

--- app/src/main/java/com/example/afinal/MainActivity.kt
+++ app/src/main/java/com/example/afinal/MainActivity.kt
@@ -6,11 +6,15 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     // Firebase Authentication
     private var mAuth: FirebaseAuth? = null
+
+    // Firebase Database
+    private var mDatabase: FirebaseDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +26,24 @@ class MainActivity : AppCompatActivity() {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance()
 
+        // Initialize Firebase Database
+        mDatabase = FirebaseDatabase.getInstance()
+
         // If user is not logged in, go to Login Page
         if (mAuth!!.currentUser == null) {
             val intent = Intent(this@MainActivity, Login::class.java)
             startActivity(intent)
             finish()
         } else {
-            // Welcome message
-            welcomeText.text = buildString {
-        append("Welcome ")
-        append(mAuth!!.currentUser!!.email)
-    }
+            // Get user information from Firebase Database
+            val user = mAuth!!.currentUser
+            val userId = user!!.uid
+            val userRef = mDatabase!!.getReference("users").child(userId)
+            userRef.get().addOnSuccessListener { snapshot ->
+                val firstName = snapshot.child("firstName").value.toString()
+                val lastName = snapshot.child("lastName").value.toString()
+                welcomeText.text = "Welcome $firstName $lastName"
+            }
         }
     }
 }
