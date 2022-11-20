1030e38c8cebfa9b4978d670a3a7ba5fabd1fe78
Alexander Burgos <axb19c@acu.edu>
2022-11-20 00:10:11 -0600

    Log In and Registration finished

--- app/src/main/java/com/example/afinal/MainActivity.kt
+++ app/src/main/java/com/example/afinal/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.afinal
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.afinal.R
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -11,21 +12,27 @@ class MainActivity : AppCompatActivity() {
     // Firebase Authentication
     private var mAuth: FirebaseAuth? = null
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // UI elements
+        val welcomeText = findViewById<TextView>(R.id.test)
+
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance()
 
-        // if user is not logged in, go to login page, else go to main page
+        // If user is not logged in, go to Login Page
         if (mAuth!!.currentUser == null) {
             val intent = Intent(this@MainActivity, Login::class.java)
             startActivity(intent)
+            finish()
         } else {
-            val intent = Intent(this@MainActivity, MainActivity::class.java)
-            startActivity(intent)
+            // Welcome message
+            welcomeText.text = buildString {
+        append("Welcome ")
+        append(mAuth!!.currentUser!!.email)
+    }
         }
     }
 }
--- app/src/main/java/com/example/afinal/Register.kt
+++ app/src/main/java/com/example/afinal/Register.kt
@@ -24,6 +24,15 @@ class Register : AppCompatActivity() {
         val username = findViewById<EditText>(R.id.register_username)
         val email = findViewById<EditText>(R.id.register_email)
         val password = findViewById<EditText>(R.id.register_password)
+        val firstName = findViewById<EditText>(R.id.register_firstname)
+        val middleName = findViewById<EditText>(R.id.register_middlename)
+        val lastName = findViewById<EditText>(R.id.register_lastname)
+        val phone = findViewById<EditText>(R.id.register_phone)
+        val addressLine1 = findViewById<EditText>(R.id.register_address_1)
+        val addressLine2 = findViewById<EditText>(R.id.register_address_2)
+        val city = findViewById<EditText>(R.id.register_city)
+        val state = findViewById<EditText>(R.id.register_state)
+        val zip = findViewById<EditText>(R.id.register_zip)
         val registerButton = findViewById<Button>(R.id.button_submit_register)
 
         // Initialize Firebase Auth
@@ -31,5 +40,46 @@ class Register : AppCompatActivity() {
 
         // Initialize Firebase Database
         mDatabase = FirebaseDatabase.getInstance()
+
+        // Register User
+        registerButton.setOnClickListener {
+
+            // Get text from UI elements
+            val usernameText = username.text.toString()
+            val emailText = email.text.toString()
+            val passwordText = password.text.toString()
+            val firstNameText = firstName.text.toString()
+            val middleNameText = middleName.text.toString()
+            val lastNameText = lastName.text.toString()
+            val addressLine1Text = addressLine1.text.toString()
+            val addressLine2Text = addressLine2.text.toString()
+            val cityText = city.text.toString()
+            val stateText = state.text.toString()
+            val zipText = zip.text.toString()
+            val phoneText = phone.text.toString()
+
+            if (usernameText.isEmpty() || emailText.isEmpty() || passwordText.isEmpty() || firstNameText.isEmpty() || lastNameText.isEmpty() || addressLine1Text.isEmpty() || cityText.isEmpty() || stateText.isEmpty() || zipText.isEmpty() || phoneText.isEmpty()) {
+                Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                mAuth!!.createUserWithEmailAndPassword(emailText, passwordText).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+
+                        // Initialize User
+                        val user = User(firstNameText, middleNameText, lastNameText, emailText, phoneText, addressLine1Text, addressLine2Text, cityText, stateText, zipText)
+
+                        // Add user to database under uid
+                        val uid = mAuth!!.currentUser!!.uid
+                        mDatabase!!.reference.child("users").child(uid).setValue(user)
+
+                        // Go back to login
+                        finish()
+                    }
+                    else {
+                        Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
