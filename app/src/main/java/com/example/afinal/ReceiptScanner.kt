da96bc02f7372f1d937045659fe3fa076c00c952
Alexander Burgos <axb19c@acu.edu>
2022-11-20 21:32:48 -0600

    Adds camera permissions and basic function templates

--- app/src/main/java/com/example/afinal/ReceiptScanner.kt
+++ app/src/main/java/com/example/afinal/ReceiptScanner.kt
@@ -1,11 +1,130 @@
 package com.example.afinal
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageProxy
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.afinal.databinding.ActivityMainBinding
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import java.util.concurrent.ExecutorService
+
+typealias LumaListener = (luma: Double) -> Unit
 
 class ReceiptScanner : AppCompatActivity() {
+
+    // Class variables
+    private var imageCapture: ImageCapture? = null
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+
+    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var cameraExecutor: ExecutorService
+
+    // Text Recognition
+    private var recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receipt_scanner)
+
+        // UI elements
+        val button_photo = findViewById<Button>(R.id.button_capture_photo)
+        val button_video = findViewById<Button>(R.id.button_capture_video)
+
+        // Initialize device's camera
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        // Get camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        // Add a listener to the Capture button
+        button_photo.setOnClickListener { takePhoto() }
+        button_video.setOnClickListener { captureVideo() }
+    }
+
+    // Function to start camera
+    private fun startCamera() {}
+
+    // Function to take a photo
+    private fun takePhoto(){}
+
+    // Function to take a video
+    private fun captureVideo(){}
+
+    // Function to analyze the photo
+    private fun analyzePhoto(){
+        // ImageAnalysis
+        fun analyze(imageProxy: ImageProxy) {
+            val mediaImage = imageProxy.image
+            if (mediaImage != null) {
+                val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+                recognizer.process(image)
+                    .addOnSuccessListener { visionText ->
+                        // Task completed successfully
+                        // ...
+                    }
+                    .addOnFailureListener { e ->
+                        // Task failed with an exception
+                        // ...
+                    }
+            }
+        }
+
+    }
+
+    // Function to check if all permissions are granted
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // Function to request permissions
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    // Function to stop camera
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "Receipt Scanner"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = mutableListOf(android.Manifest.permission.CAMERA).toTypedArray()
     }
 }
